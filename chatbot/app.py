1aeffd5fd86a890c1ea81a84705cad951aeb0325
우다현 <wdh112139@gmail.com>
2021-06-08 21:31:04 +0900

    기본적인 rest api

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, request,jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello Flask'
+# 서버 리소스
+resource = []
 
-@app.route('/info/<name>')
-def get_name(name):
-    return "hello {}".format(name)
+# 사용자 정보 조회
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
 
-@app.route('/user/<int:id>')
-def get_user(id):
-    return "user id is {}".format(id)
+    for user in resource:
+        if user['user_id'] is user_id:
+            return jsonify(user)
+    
+    return jsonify(None)
+
+# 사용자 추가
+@app.route('/user', methods=['POST'])
+def add_user(user_id):
+    user = request.get_json()
+    resource.append(user)
+    return jsonify(resource)
+
 
 @app.route('/json/<int:dest_id>/<message>')
 @app.route('/JSON/<int:dest_id>/<message>')
