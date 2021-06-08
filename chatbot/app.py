60b0e95bf8ab9e2ae7bcbd7235e6a2aa9a1a432f
우다현 <wdh112139@gmail.com>
2021-06-08 21:05:09 +0900

    uri 동적 변수 연습

--- chatbot/app.py
+++ chatbot/app.py
@@ -5,5 +5,22 @@ app = Flask(__name__)
 def hello():
     return 'Hello Flask'
 
+@app.route('/info/<name>')
+def get_name(name):
+    return "hello {}".format(name)
+
+@app.route('/user/<int:id>')
+def get_user(id):
+    return "user id is {}".format(id)
+
+@app.route('/json/<int:dest_id>/<message>')
+@app.route('/JSON/<int:dest_id>/<message>')
+def send_message(dest_id,message):
+    json = {
+        "bot_id": dest_id,
+        "message": send_message
+    }
+    return json
+
 if __name__ == '__main__':
     app.run()
