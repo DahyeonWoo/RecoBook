286fc2f95da84543157ed3e3bfedf2aba78eeeb0
Haebuk <cwryu6252@naver.com>
2021-08-18 10:47:40 +0900

    ColumsFromDB 완성

--- project/flask/api/utils/ColumnsFromDB.py
+++ project/flask/api/utils/ColumnsFromDB.py
@@ -1,14 +1,10 @@
 import sys
 sys.path.append('./project/flask/')
 from api.db_model.mysql import conn_mysqldb
+from CreateDict import create_dict
 
-class name:
-    def __init__(self, table_name, db_col, col, param):
-        self.tabel_name = table_name
-        self.db_col = db_col
-        self.col = col
-        self.param = param
-        
+class ColumnsFromDB:
+    @staticmethod   
     def get_col_name(table_name):
         """
         해당 테이블의 컬럼명을 반환하는 함수
@@ -26,8 +22,12 @@ class name:
         db_cursor.execute(sql)
         col_name_list = db_cursor.fetchall()
         db.close()
-        return col_name_list
+        total_col_list = []
+        for col_name in col_name_list:
+            total_col_list.append(col_name[0])
+        return total_col_list
 
+    @staticmethod
     def get_db_data(db_col, table_name, col, param):
             """
             지정한 컬럼에 관한 정보를 불러오는 함수
@@ -35,7 +35,6 @@ class name:
             :params table_name: 테이블명
             :params col: 
             :params param: 조건문에 들어갈 이름
-            
 
             :return: 리스트
             """
@@ -44,12 +43,25 @@ class name:
             sql = f"SELECT {db_col} FROM {table_name} WHERE {col} LIKE REPLACE('%{param}%', ' ', '')"
             db_cursor.execute(sql)
             db_data = db_cursor.fetchone()
-            return db_data
+            total_db_col = ColumnsFromDB.get_col_name(table_name)
+            col_list = db_col.split(',')
+            dict = create_dict()
+            if db_col == '*':
+                for i in range(len(total_db_col)):
+                    dict.add(total_db_col[i], db_data[i])
+            else:
+                for i, col in enumerate(col_list):
+                    if col.strip() in total_db_col:
+                        dict.add(col.strip(), db_data[i])
+                    else:
+                        print('부적절한 col, 문법 체크')
+            return dict
 
 if __name__ == '__main__':
-    # res = get_db_data('*', 'Book', 'title', '미스테리아') # done
+    res = ColumnsFromDB.get_db_data('*', 'Book', 'title', '미스테리아') # done
     # print(res)
     # res = get_db_data('isbn13, title', 'Book', 'title', '미스테리아') # done
     # print(res)
-    res = get_col_name('Book')
-    print(res[0])
+    # res = ColumnsFromDB.get_col_name('Book')
+    # res = ColumnsFromDB.get_db_data('isbn13, title', 'Book', 'title', '미스테리아')
+    print(res)
