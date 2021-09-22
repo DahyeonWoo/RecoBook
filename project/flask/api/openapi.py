4582fde01cb4d45c3b0bb61bde25775063e6626f
Haebuk <cwryu6252@naver.com>
2021-09-22 16:24:54 +0900

    책 수집

--- project/flask/api/openapi.py
+++ project/flask/api/openapi.py
@@ -13,19 +13,17 @@ class Aladin:
     @staticmethod
     def post_book():
         db = pymysql.connect(host=config('host'),port=3306,user=config('user'),passwd=config('passwd'),db=config('db'),charset='utf8mb4')
-        category_id = 40173
+        category_id = 2225
         """ 
-        656인문학, 798사회과학, 50935로맨스, 50930과학소설(SF), 50931호러.공포소설, 50932무협소설
+        656인문학, 798사회과학, 50935 로맨스, 50930 과학소설(SF), 50931 호러.공포소설, 50932 무협소설
         50933 액션/스릴러소설, 50940 시, 2225 재테크/투자 일반, 1632 마케팅/브랜드, 987	과학
-        2105 고전, 2125	서양고전문학, 2951	인간관계, 40173	시간관리
+        2105 고전, 2125	서양고전문학, 2951 인간관계, 40173	시간관리
 
-
- 
         """
         ttbkey = config('TTBKEY')
         for starting_page in range(1,10):
             print('starting_page:', starting_page)
-            url = f"http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey={str(ttbkey)}&QueryType=Bestseller&MaxResults=50&start={starting_page}&SearchTarget=Book&output=js&Version=20131101&CategoryId={category_id}" # 8번행 건너뜀 # 18번 error #21번 건너뜀 # 시나공 책 하나 제외 21번 중복 # 22번 중복
+            url = f"http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey={str(ttbkey)}&QueryType=ItemNewSpecial&MaxResults=50&start={starting_page}&SearchTarget=Book&output=js&Version=20131101&CategoryId={category_id}" # 8번행 건너뜀 # 18번 error #21번 건너뜀 # 시나공 책 하나 제외 21번 중복 # 22번 중복
             db_cursor = db.cursor()
             bookList = requests.get(url).json()
             # print(url)
@@ -47,7 +45,10 @@ class Aladin:
                 adult = bookList['item'][i]['adult'] #bool
                 customerReviewRank = bookList['item'][i]['customerReviewRank'] #int
                 category = bookList['item'][i]['categoryName'] #str
-                bestRank = bookList['item'][i]['bestRank'] #int
+                try:
+                    bestRank = bookList['item'][i]['bestRank'] #int
+                except Exception as e:
+                    bestRank = 0 # 신착자료 bestRank 없음: 0으로 채움
 
                             
                 sql = """INSERT INTO Book(isbn13,title,link,author,publishDate,description,publisher,priceStandard,stockStatus,cover,salesPoint,adult,customerReviewRank,category,bestRank) 
