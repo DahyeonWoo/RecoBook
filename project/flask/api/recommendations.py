3b9df26b115309dabf36f3569a25759958f06c0e
(유수빈) <65115411+ssubin29@users.noreply.github.com>
2021-10-17 09:35:41 +0900

    Update recommendations.py

--- project/flask/api/recommendations.py
+++ project/flask/api/recommendations.py
@@ -128,13 +128,17 @@ class NLPRecommend:
         db_cursor = db.cursor()
 
         try:
-            sql = f"SELECT bookWant FROM User WHERE idx = {user_idx};"
+            sql = f"SELECT bookWant, BookRead FROM User WHERE idx = {user_idx};"
             db_cursor.execute(sql)
             result = list(db_cursor.fetchall()[0])
-            # print('bookWant:',result)        
+            # print('bookWant:',result)      
 
-            bookWantList = result.split(';')
-            bookWantList = ['\''+bookWant.strip()+'\'' for bookWant in bookWantList]            
+            bookWantList = []
+            for book in result:
+                bookWantList.extend(book.split(';'))
+
+            bookWantList = list(set(['\''+bookWant.strip()+'\'' for bookWant in bookWantList]   ))  
+
             sql = f"SELECT isbn13,title,vector FROM Review WHERE title LIKE {' OR title LIKE '.join(bookWantList)};"
             # print(sql)
             db_cursor.execute(sql)  
